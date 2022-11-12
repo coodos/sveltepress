19e5efe73e68493f9b39c706dc794af2c0bac1d8
Coodos <info@coodos.co>
2022-11-12 18:30:44 +0530

    fead: add pogybook

---
+++ client/src/components/ui/Button/Button.stories.svelte
@@ -0,0 +1,27 @@
+<script>
+    import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+    import { ButtonSizePresets, ButtonTypes } from "../../../types";
+    import Button from "./Button.svelte";
+
+    const args = {
+        variant: ButtonTypes.Primary,
+        size: ButtonSizePresets.Medium,
+        label: "hi button",
+        onClick: () => {},
+    };
+
+    const secondaryArgs = {
+        ...args,
+        variant: ButtonTypes.Secondary,
+    };
+</script>
+
+<Meta title="Button" component={Button} />
+
+<Template let:args>
+    <Button {...args} />
+</Template>
+
+<Story name="Primary" {args} />
+
+<Story name="Secondary" args={secondaryArgs} />
