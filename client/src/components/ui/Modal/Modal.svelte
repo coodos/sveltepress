ad7859fa757f6960d3d222fa7b0bd263e03c3105
Coodos <info@coodos.co>
2022-11-13 10:58:40 +0530

    feat: modal UwU

---
+++ client/src/components/ui/Modal/Modal.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import { Card } from "../";
+
+    export let isOpen: boolean = false;
+    export let setIsOpen: (isOpen: boolean) => void;
+</script>
+
+{#if isOpen}
+    <div class="backdrop" on:click={() => setIsOpen(false)} />
+    <div class="modal">
+        <Card>
+            <slot name="buttons">
+                <button class="close-button" on:click={() => setIsOpen(false)}
+                    >x</button
+                ></slot
+            >
+            <div class="modal-header">
+                <slot name="header" />
+            </div>
+            <slot />
+        </Card>
+    </div>
+{/if}
--- client/src/components/ui/index.ts
+++ client/src/components/ui/index.ts
@@ -1,1 +1,2 @@
 export { default as Button } from "./Button/Button.svelte";
+export { default as Card } from "./Card/Card.svelte";
