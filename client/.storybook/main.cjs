4b1311de2af3eea330d2ae17d7701115dce99444
Coodos <info@coodos.co>
2022-11-15 17:31:55 +0530

    chore: delete `client` (previously svelte) and rename `frontend` (new sveltekit dir) to `client`

--- client/.storybook/main.cjs
+++ client/.storybook/main.cjs
@@ -1,23 +1,45 @@
-const preprocess = require("svelte-preprocess");
+// module.exports = {
+//   "stories": [
+//     "../src/**/*.stories.mdx",
+//     "../src/**/*.stories.@(js|jsx|ts|tsx|svelte)"
+//   ],
+//   "addons": [
+//     "@storybook/addon-links",
+//     "@storybook/addon-essentials",
+//     "@storybook/addon-interactions"
+//   ],
+//   "framework": "@storybook/svelte",
+//   "core": {
+//     "builder": "@storybook/builder-vite"
+//   },
+//   "svelteOptions": {
+//     "preprocess": require("../svelte.config.js").preprocess
+//   },
+//   "features": {
+//     "storyStoreV7": true
+//   }
+// }
+
+const preprocess = require('svelte-preprocess');
 
 module.exports = {
-    stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(ts|svelte)"],
-    addons: [
-        "@storybook/addon-links",
-        "@storybook/addon-essentials",
-        "@storybook/addon-interactions",
-        "@storybook/addon-svelte-csf",
-    ],
-    framework: "@storybook/svelte",
-    core: {
-        builder: "@storybook/builder-vite",
-    },
-    svelteOptions: {
-        preprocess: preprocess({
-            typescript: true,
-        }),
-    },
-    features: {
-        storyStoreV7: false,
-    },
+	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(ts|svelte)'],
+	addons: [
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@storybook/addon-interactions',
+		'@storybook/addon-svelte-csf'
+	],
+	framework: '@storybook/svelte',
+	core: {
+		builder: '@storybook/builder-vite'
+	},
+	svelteOptions: {
+		preprocess: preprocess({
+			typescript: true
+		})
+	},
+	features: {
+		storyStoreV7: false
+	}
 };
