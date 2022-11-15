add5be4d291608b9c354d735045d80408cc707df
Merul <69296233+coodos@users.noreply.github.com>
2022-11-15 19:35:37 +0530

    Feat/sveltekit express migration (#2)
    * feat: add sveltekit
    
    * chore: migrate components
    
    * feat: migrate storybook (fucking finally this shit works)
    
    * chore: delete `client` (previously svelte) and rename `frontend` (new sveltekit dir) to `client`
    
    * chore: cleanup `.storybook/main.cjs`
    
    * chore: remove storybook propoganda

--- client/.storybook/main.cjs
+++ client/.storybook/main.cjs
@@ -1,23 +1,23 @@
-const preprocess = require("svelte-preprocess");
+const preprocess = require('svelte-preprocess');
 
 module.exports = {
-    stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(ts|svelte)"],
-    addons: [
-        "@storybook/addon-links",
-        "@storybook/addon-essentials",
-        "@storybook/addon-interactions",
-        "@storybook/addon-svelte-csf",
-    ],
-    framework: "@storybook/svelte",
-    core: {
-        builder: "@storybook/builder-vite",
-    },
-    svelteOptions: {
-        preprocess: preprocess({
-            typescript: true,
-        }),
-    },
-    features: {
-        storyStoreV7: false,
-    },
+	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(ts|svelte)'],
+	addons: [
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@storybook/addon-interactions',
+		'@storybook/addon-svelte-csf'
+	],
+	framework: '@storybook/svelte',
+	core: {
+		builder: '@storybook/builder-vite'
+	},
+	svelteOptions: {
+		preprocess: preprocess({
+			typescript: true
+		})
+	},
+	features: {
+		storyStoreV7: false
+	}
 };
