e3c1f2c9dec5cc466be852a950a125f56c65583d
Coodos <info@coodos.co>
2022-11-15 17:23:29 +0530

    chore: migrate components

---
+++ frontend/src/lib/components/ui/Button/Button.stories.svelte
@@ -0,0 +1,26 @@
+<script>
+    import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+    import Button from "./Button.svelte";
+
+    const args = {
+        variant: "primary",
+        size: "medium",
+        label: "hi button",
+        onClick: () => {},
+    };
+
+    const secondaryArgs = {
+        ...args,
+        variant: "secondary",
+    };
+</script>
+
+<Meta title="Button" component={Button} />
+
+<Template let:args>
+    <Button {...args} />
+</Template>
+
+<Story name="Primary" {args} />
+
+<Story name="Secondary" args={secondaryArgs} />
