9a50b17ec7ca1ab023f030a3005245b02239ad31
Coodos <info@coodos.co>
2022-11-24 06:47:09 +0530

    feat: fix login stuff

--- client/src/routes/register/+page.svelte
+++ client/src/routes/register/+page.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
     import { Card, TextInput, Button } from '$lib/components/ui'
+	import { axios } from '$lib/utils/axios.utils';
 
     let email: string;
     let password: string;
 
-    const onClick = () => {
-        console.log(email, password)
+    const onClick = async () => {
+        const { data } = await axios.post("/users", { email, password})
+        window.location.pathname = "/login";
     }
 </script>
 
