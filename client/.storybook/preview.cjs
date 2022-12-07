d097e2e58f0f479564b62f434eccd803c67f7228
Coodos <info@coodos.co>
2022-12-07 09:03:03 +0530

    feat: storybook integration with darkmode

--- client/.storybook/preview.cjs
+++ client/.storybook/preview.cjs
@@ -1,7 +1,14 @@
 import "../static/style.css";
+import { themes } from "@storybook/theming";
 
 export const parameters = {
 	actions: { argTypesRegex: "^on[A-Z].*" },
+	darkmode: {
+		darkClass: "dark",
+		classTarget: "body",
+		current: "light",
+		dark: { ...themes.dark, appBg: "black" }
+	},
 	controls: {
 		matchers: {
 			color: /(background|color)$/i,
