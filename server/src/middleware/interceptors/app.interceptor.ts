9bc61acb42cba35e8992ae01570b27cc9a2c8b9a
Coodos <info@coodos.co>
2022-12-18 10:40:52 +0530

    fix: app interceptor breaking non-json content

--- server/src/middleware/interceptors/app.interceptor.ts
+++ server/src/middleware/interceptors/app.interceptor.ts
@@ -2,7 +2,9 @@ import { FILTER_KEYS } from "@/config";
 import { sanitizeObject } from "@/utils";
 import { responseHandler } from "express-intercept";
 
-export const AppInterceptor = responseHandler().replaceBuffer((body, res) => {
+export const AppInterceptor = responseHandler().replaceBuffer((body, req) => {
+	if (!req.headers.accept.includes("application/json")) return body;
+
 	const sanitizedObject = sanitizeObject(
 		JSON.parse(body.toString()),
 		FILTER_KEYS.split(",").map((e) => e.trim())
